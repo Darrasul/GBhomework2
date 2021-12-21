0bdf2cc80522ced670cd4e01caab9acb6168b8fd
Vitovtas Buzas <vitavtasbuzas@gmail.com>
2021-12-21 10:15:26 +0300

    Done at 10:14 21.12.2021

---
+++ src/geekbrains/hmwrk2/lesson2/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package geekbrains.hmwrk2.lesson2;
+
+public class MyArrayDataException extends Exception{
+    public MyArrayDataException() {
+        super("Cant use parseInt for some reason");
+    }
+}
---
+++ src/geekbrains/hmwrk2/lesson2/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package geekbrains.hmwrk2.lesson2;
+
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException() {
+        super("Wrong massive size");
+    }
+}
