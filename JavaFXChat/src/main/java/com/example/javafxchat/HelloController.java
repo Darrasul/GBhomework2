a47126a67d7659775b2092fbb43841c820cd33dc
Vitovtas Buzas <vitavtasbuzas@gmail.com>
2021-12-28 09:56:50 +0300

    Done at 9:56 28.12.2021

---
+++ JavaFXChat/src/main/java/com/example/javafxchat/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.javafxchat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class HelloController {
+
+
+    @FXML private TextArea textArea;
+    @FXML private TextField textField;
+    @FXML private Button sendButton;
+    @FXML public ListView<String> userList;
+
+    public void sendMessage(ActionEvent actionEvent) {
+        if (!textField.getText().isEmpty()) {
+            textArea.appendText(textField.getText());
+            textArea.appendText(System.lineSeparator());
+            textField.clear();
+        }
+    }
+}
---
+++ src/geekbrains/hmwrk2/MainFourthLesson.java
@@ -0,0 +1,13 @@
+package geekbrains.hmwrk2;
+
+import geekbrains.hmwrk2.lesson4.ChatWindow;
+
+import javax.swing.*;
+
+public class MainFourthLesson {
+    public static void main(String[] args) {
+//        SwingUtilities.invokeLater(ChatWindow:: new); - запомнить для темы по многопоточности
+        new ChatWindow();
+    }
+
+}
