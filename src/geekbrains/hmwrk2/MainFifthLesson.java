9d905f06365dc018129e63641f92f0cd7390311e
Vitovtas Buzas <vitavtasbuzas@gmail.com>
2022-01-05 22:36:24 +0300

    Addition to lesson5 homework

--- src/geekbrains/hmwrk2/MainFifthLesson.java
+++ src/geekbrains/hmwrk2/MainFifthLesson.java
@@ -1,8 +1,8 @@
 package geekbrains.hmwrk2;
 
 public class MainFifthLesson {
-    private static int fullArraySize = 1000000;
-    private static int halfArraySize = fullArraySize / 2;
+    private static final int FULL_ARRAY_SIZE = 10_000_000;
+    private static final int HALF_ARRAY_SIZE = FULL_ARRAY_SIZE / 2;
 
     public static void main(String[] args) {
         fullArrayCount();
@@ -18,7 +18,7 @@ public class MainFifthLesson {
     }
 
     private static void fullArrayCount() {
-        float[] floatArray = new float[fullArraySize];
+        float[] floatArray = new float[FULL_ARRAY_SIZE];
         for (int i = 0; i < floatArray.length; i++) {
             floatArray[i] = 1.0f;
         }
@@ -31,19 +31,19 @@ public class MainFifthLesson {
 
 
     private static void dividedArrayCount() {
-        float[] dividedArray = new float[fullArraySize];
+        float[] dividedArray = new float[FULL_ARRAY_SIZE];
         for (int i = 0; i < dividedArray.length; i++) {
             dividedArray[i] = 1.0f;
         }
 
         long startTime = System.currentTimeMillis();
 
-        float[] firstHalfOfArray = new float[halfArraySize];
-        float[] secondHalfOfArray = new float[halfArraySize];
-        float[] mergedArray = new float[fullArraySize];
+        float[] firstHalfOfArray = new float[HALF_ARRAY_SIZE];
+        float[] secondHalfOfArray = new float[HALF_ARRAY_SIZE];
+        float[] mergedArray = new float[FULL_ARRAY_SIZE];
 
-        System.arraycopy(dividedArray, 0, firstHalfOfArray, 0, halfArraySize);
-        System.arraycopy(dividedArray, halfArraySize, secondHalfOfArray, 0, halfArraySize);
+        System.arraycopy(dividedArray, 0, firstHalfOfArray, 0, HALF_ARRAY_SIZE);
+        System.arraycopy(dividedArray, HALF_ARRAY_SIZE, secondHalfOfArray, 0, HALF_ARRAY_SIZE);
         long divideTime = System.currentTimeMillis();
         System.out.println("Divide time is " + (divideTime - startTime) + " ms");
 
@@ -53,11 +53,13 @@ public class MainFifthLesson {
         Thread thread2 = new Thread(() -> {
             arrayRendering(secondHalfOfArray);
         });
+        thread1.start();
+        thread2.start();
         long renderTime = System.currentTimeMillis();
         System.out.println("Render time is " + (renderTime - divideTime));
 
-        System.arraycopy(firstHalfOfArray, 0, mergedArray, 0, halfArraySize);
-        System.arraycopy(secondHalfOfArray, 0, mergedArray, halfArraySize, halfArraySize);
+        System.arraycopy(firstHalfOfArray, 0, mergedArray, 0, HALF_ARRAY_SIZE);
+        System.arraycopy(secondHalfOfArray, 0, mergedArray, HALF_ARRAY_SIZE, HALF_ARRAY_SIZE);
         System.out.println("Merge time is " + (System.currentTimeMillis() - renderTime));
         System.out.println("Two thread time is " + (System.currentTimeMillis() - startTime) + " ms");
     }
